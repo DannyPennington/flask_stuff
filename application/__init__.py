6d8c447fd380a43c9e7031624a65b976c97e3734
Ubuntu <danny@flaskdata.hm0zmdqp42cendv42rp103kfjg.zx.internal.cloudapp.net>
2020-02-24 16:36:45 +0000

    progress updates

--- application/__init__.py
+++ application/__init__.py
@@ -3,8 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = str(os.getenv('DATABASE_URI'))
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
+#app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://danny:Linuxdatabase1@testbase2.mysql.database.azure.com/testing"
 #os.getenv('DATABASE_URI')
 
 db = SQLAlchemy(app)
--- create.py
+++ create.py
@@ -1,3 +1,5 @@
 from application import db
 
 db.create_all()
+
+#fake data for change
