652ce134209600ae4be2fe891e1d5c45fa496ee4
Ubuntu <danny@Flasklinux.qnw3a55ntw3e5aszwxni5vealf.zx.internal.cloudapp.net>
2020-02-25 14:30:40 +0000

    Site now has log on and home page displays posts from database

--- application/__init__.py
+++ application/__init__.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 #app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
 app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://danny@flaskbase2:Linuxdatabase1@flaskbase2.mysql.database.azure.com/testing"
 #os.getenv('DATABASE_URI')
+app.config['SECRET_KEY'] = str(os.getenv("MY_SECRET_KEY"))
 
 db = SQLAlchemy(app)
 
--- application/routes.py
+++ application/routes.py
@@ -1,12 +1,15 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
 
-from application import app
+from application import app, db
+from application.models import Posts
+from application.forms import PostForm
 
 @app.route('/')
 @app.route('/home')
 
 def home():
-    return render_template('home.html', title='Home')
+    postData = Posts.query.all()
+    return render_template('home.html', title='Home', posts=postData)
 
 @app.route('/about')
 def about():
@@ -20,3 +23,23 @@ def login():
 def register():
     return render_template('register.html',title ='Registration')
 
+@app.route('/post', methods=['GET','POST'])
+def post():
+    form = PostForm()
+    if form.validate_on_submit():
+        postData = Posts(
+            first_name = form.first_name.data,
+            last_name = form.last_name.data,
+            title = form.title.data,
+            content = form.content.data
+        )
+
+        db.session.add(postData)
+        db.session.commit()
+
+        return redirect(url_for('home'))
+
+    else:
+        print(form.errors)
+
+    return render_template('post.html', title='Post', form=form)
